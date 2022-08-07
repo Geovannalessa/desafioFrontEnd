149e70ea5e2631ceb8e838a3039d504250413e40
Geovanna Lessa <geovannalessa2@gmail.com>
2022-08-07 12:45:09 -0300

    css semi finalizado

--- reactpokemon/src/App.js
+++ reactpokemon/src/App.js
@@ -45,19 +45,53 @@ function App() {
           <article className="nomes">
             <a href='#'>Charmander</a>
           </article>
-          
-        
+          <button>BACK</button>
+          <button>NEXT</button>
+
         </section>
       </section>
 
         <section className="habilidades">
           <Images/>
-          <section>
+          <section className= "titulo">
           Rattata
           </section>
-          <section>
-            
+          <section className= "quadrado-habilidades">
+            <div>
+              grass
+            </div>
+            <div>
+              grass
+            </div>
           </section>
+          <article className="powers">
+          <table>
+            <tr>
+              hp
+              <td> 
+                <td className="hp"></td>
+              </td>
+            </tr>
+            <tr>
+              attack
+              <td> 
+                <td className="attack"></td>
+              </td>
+            </tr>
+            <tr>
+              defense
+              <td> 
+                <td className="defense"></td>
+              </td>
+            </tr>
+            <tr>
+              speed
+              <td> 
+                <td className="speed"></td>
+              </td>
+            </tr>
+          </table>
+          </article>
         </section>
 
 
