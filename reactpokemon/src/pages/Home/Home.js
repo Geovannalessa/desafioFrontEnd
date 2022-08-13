8c35973915b161fec66fed47879c49ce754de03a
Geovanna Lessa <geovannalessa2@gmail.com>
2022-08-13 14:52:56 -0300

    Tela criado para caso não escolher nenhum personagem

--- reactpokemon/src/pages/Home/Home.js
+++ reactpokemon/src/pages/Home/Home.js
@@ -1,48 +1,9 @@
+import './Home.css'
 function Home(){
     return(
-        <main>
-             <section className="habilidades">
-        
-        <section className="titulo">
-          Rattata
-        </section>
-        <section className="quadrado-habilidades">
-          <div>
-            grass
-          </div>
-          <div>
-            grass
-          </div>
-        </section>
-        <article className="powers">
-          <table>
-            <tr>
-              hp
-              <td>
-                <td className="hp"></td>
-              </td>
-            </tr>
-            <tr>
-              attack
-              <td>
-                <td className="attack"></td>
-              </td>
-            </tr>
-            <tr>
-              defense
-              <td>
-                <td className="defense"></td>
-              </td>
-            </tr>
-            <tr>
-              speed
-              <td>
-                <td className="speed"></td>
-              </td>
-            </tr>
-          </table>
-        </article>
-      </section>
+        <main className='choose'>
+             <h2>Choose a 
+                 POKEMON</h2>
         </main>
     )
 }
